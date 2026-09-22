b01f80a590f8b01ac06050423ba1b1e379a9d394
Lethabo <maphotokgethi@gmail.com>
2026-09-22 06:04:12 +0200

    Add files via upload

---
+++ source/buildConfig/androidTest/debug/com/groupeight/safezonesa/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.groupeight.safezonesa.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.groupeight.safezonesa.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String API_BASE_URL = "https://safezone-sa-api-2026-dffnf6ahhtchcxcn.centralindia-01.azurewebsites.net/";
+}
---
+++ source/buildConfig/debug/com/groupeight/safezonesa/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.groupeight.safezonesa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.groupeight.safezonesa";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0-part1-prototype";
+  // Field from build type: debug
+  public static final String API_BASE_URL = "https://safezone-sa-api-2026-dffnf6ahhtchcxcn.centralindia-01.azurewebsites.net/";
+}
